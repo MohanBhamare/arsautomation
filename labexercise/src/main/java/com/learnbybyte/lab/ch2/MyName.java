eccaf1cc61923b1c71da6bcd72c597bfb76d5cef
mohanbhamare <mohan.bhamare@gmail.com>
2017-03-19 22:48:42 +0530

    lab ch2

---
+++ labexercise/src/main/java/com/learnbybyte/lab/ch2/MyName.java
@@ -0,0 +1,11 @@
+package com.learnbybyte.lab.ch2;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("MyName|main|");
+      System.out.println("MyName|main|"+"Mohan Bhamare");
+	}
+
+}
