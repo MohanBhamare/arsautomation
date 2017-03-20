bbcec39facea81e7a64895fe12b5d52711ac9c1c
mohanbhamare <mohan.bhamare@gmail.com>
2017-03-20 08:34:29 +0530

    LegalEntity interface created in java

---
+++ arsautomation/src/test/java/com/sumdev/tax/ars/arsauto/repo/ILegalEntityRepository.java
@@ -0,0 +1,13 @@
+package com.sumdev.tax.ars.arsauto.repo;
+import com.sumdev.tax.ars.arsauto.vo.LegalEntity;
+
+public interface ILegalEntityRepository {
+
+public void open();
+
+public void insert(LegalEntity le);
+
+public void close();
+
+	
+}
